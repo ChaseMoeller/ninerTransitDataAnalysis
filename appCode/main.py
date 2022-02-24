954b5fec302b3fdf869db06c44dbfbc4f977702d
ChaseMoeller <chase@moellerweb.com>
2022-02-24 13:50:18 -0500

    added index.html, defaultStyle.css, and main.py

---
+++ appCode/main.py
@@ -0,0 +1,15 @@
+﻿import os
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect
+from flask import url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
