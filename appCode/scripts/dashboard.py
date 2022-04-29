dc6281e9335d4652afb3ad77090b3558f48d6158
camojoe215 <camojoe215@gmail.com>
2022-04-29 13:18:55 -0400

    working dynamic rendering

--- appCode/scripts/dashboard.py
+++ appCode/scripts/dashboard.py
@@ -7,6 +7,8 @@ from dash import dcc, html, Input, Output, dash_table
 
 
 def create_dashboard(server):
+
+    #setup dash
     dash_app = dash.Dash(
         server=server,
         routes_pathname_prefix='/dashapp/',
@@ -15,11 +17,14 @@ def create_dashboard(server):
 
     #upload file
     df = pd.read_excel('appCode/niner-transit-data/Untitled spreadsheet-2.xlsx')
+
+    #declare arrays
     sortOptions = []
     filterOptions = []
     stopNames = []
     busNumbers = []
 
+    #load arrays
     for col in df.columns:
         if col != 'Route' and col != 'Stop' and col != 'Bus':
             sortOptions.append({'label':'{}'.format(col, col),
@@ -27,7 +32,6 @@ def create_dashboard(server):
         else:
             filterOptions.append({'label':'{}'.format(col, col),
             'value':col})
-
 
     npStop = df['Stop'].to_numpy()
     npBus = df['Bus'].to_numpy()
@@ -40,67 +44,77 @@ def create_dashboard(server):
         if bus not in busNumbers:
             busNumbers.append(bus)
 
-    opt = stopNames
-    #default graph
+
+    #load default graph
     fig = px.line_geo(df, lat="Latitude", lon="Longitude")
-    #options
 
+    #tests
+    #print(stopNames)
+    #print(filterOptions)
 
-
-
-    print(stopNames)
-    print(filterOptions)
     #html layout
     dash_app.layout = html.Div(children=[
 
-    html.Div([
-        dcc.RadioItems(
-            id='routes',
-            options = [{'label': i, 'value': i} for i in ['Silver', 'Gold', 'Green']],
-            value= 'Silver'
-        ),
-        dcc.RadioItems(
-            id='stops',
-            options = stopNames,
-            value= stopNames[0]
-        ),
-        dcc.RadioItems(
-            id='buses',
-            options = busNumbers,
-            value= busNumbers[0]
-        )
+#filter selections
+        html.Div(id='routeSelections', children=[
+            dcc.RadioItems(
+                id='routes',
+                options = [{'label': i, 'value': i} for i in ['Silver', 'Gold', 'Green']],
+                value= 'Silver'
+            )
+        ], style={'display':'block'}),
+    
+        html.Div(id='stopSelections', children=[
+            dcc.RadioItems(
+                id='stops',
+                options = stopNames,
+                value= stopNames[0]
+            )
+        ], style={'display':'block'}),
 
-    ]),
-    html.Div([
-        dcc.Dropdown(
-            id='graph-type',
-            options = [{'label': i, 'value': i} for i in ['Map', 'Bar', 'Line', 'Pie']],
-            value='Map',
-            placeholder='Choose Graph Type'
-        ),
-        dcc.Dropdown(
-            id='filter-dd',
-            options=filterOptions,
-            placeholder='Choose Filter'
-        ),
-        dcc.Dropdown(
-            id='x-axis-dd',
-            options=sortOptions,
-            placeholder='Choose X Variable'
-        ),
-        dcc.Dropdown(
-            id='y-axis-dd',
-            options=sortOptions,
-            placeholder='Choose Y Variable'
-        )
-    ]),
-    html.Div([
-        dcc.Graph(
-            id='graph-1',
-            figure=fig
-        )
+        html.Div(id='busSelections', children=[
+            dcc.RadioItems(
+                id='buses',
+                options = busNumbers,
+                value= busNumbers[0]
+            )
+        ], style={'display':'block'}),
+
+    #Dropdowns for graph, filter, and x/y
+        html.Div([
+            dcc.Dropdown(
+                id='graph-type',
+                options = [{'label': i, 'value': i} for i in ['Map', 'Bar', 'Line', 'Pie']],
+                value='Map',
+                placeholder='Choose Graph Type'
+            ),
+            dcc.Dropdown(
+                id='filter-dd',
+                options=filterOptions,
+                value = 'Route',
+                placeholder='Choose Filter'
+            ),
+            dcc.Dropdown(
+                id='x-axis-dd',
+                options=sortOptions,
+                value = 'Latitude',
+                placeholder='Choose X Variable'
+            ),
+            dcc.Dropdown(
+                id='y-axis-dd',
+                options=sortOptions,
+                value = 'Longitude',
+                placeholder='Choose Y Variable'
+            )
+        ]),
+    #graph
+        html.Div([
+            dcc.Graph(
+                id='graph-1',
+                figure=fig
+            )
+        ])
     ])
-])
     
     @dash_app.callback(
         Output(component_id='graph-1', component_property='figure'),
@@ -113,8 +127,9 @@ def create_dashboard(server):
         Input(component_id='y-axis-dd', component_property='value')
     )
     #The parameters below correspond to the Input's above respectively
-    def update_graph(graph, filter, routes, stops, buses, xaxis, yaxis):
+    def updateGraph(graph, filter, routes, stops, buses, xaxis, yaxis):
 
+        
         if(filter == 'Route'):
             dff = df[df[filter] == routes]
         elif(filter == 'Stop'):
@@ -132,5 +147,21 @@ def create_dashboard(server):
             fig = px.pie(values=[xaxis, yaxis], names=[xaxis, yaxis])
         return fig
 
+    @dash_app.callback(
+        Output(component_id='routeSelections', component_property='style'),
+        Output(component_id='stopSelections', component_property='style'),
+        Output(component_id='busSelections', component_property='style'),
+        Input(component_id='filter-dd', component_property='value')
+    )
+    def updateFilterSelections(filter):
+        if(filter=='Route'):
+            return {'display':'block'}, {'display':'none'}, {'display':'none'}
+        elif(filter=='Stop'):
+            return {'display':'none'}, {'display':'block'}, {'display':'none'}
+        elif(filter=='Bus'):
+            return {'display':'none'}, {'display':'none'}, {'display':'block'}
+
+        return {'display':'block'}, {'display':'none'}, {'display':'none'} #show route selection if anything fails
+    
     return dash_app.server
 
